28af766aed4a24d420749ce0388d7ebb27f412c2
dldnjswns516 <dldnjswns516@naver.com>
2020-04-01 17:32:20 +0900

    [그룹웨어] 이원준 메신저 채팅방 유저초대 모달작업 완료

--- team02groupware/src/main/resources/static/messenger/js/front.js
+++ team02groupware/src/main/resources/static/messenger/js/front.js
@@ -1,9 +1,9 @@
 /**
  * 메신저 화면단 스크립트 작업
  */
-$(function(){
+
 			
-	// 채팅방 리스트 더블클릭, 채팅방 상세보기
+	// 채팅방 클릭
 	$('.chat-list-room').click(function(){
 		
 		$('.chat-list-room').css('background-color','#fff');
@@ -21,28 +21,35 @@ $(function(){
 			$('.chat-list-room').removeClass('active')
 			$(this).addClass('active')
 			
-			var chatRoom = {};
-			chatRoom.title = $(this).find('.chat-list-title').text();
-			chatRoom.user = $(this).find('.chat-list-room-user').text();
+			var roomInfo = {};
+			roomInfo.title = $(this).find('.chat-list-title').text();
+			roomInfo.user = $(this).find('.chat-list-room-user').text();
 			
-			fn_chatRoomView(chatRoom);
+			fn_chatRoomView(roomInfo);
 		}
 
 	});
 	
+	// 채팅방 상세보기
+	function fn_chatRoomView(obj){
+		
+		var chatRoom = $('.chat-room');
+		chatRoom.find('.chat-room-title').text(obj.title);
+		chatRoom.find('.chat-room-user').text(obj.user);
+		chatRoom.find('.chat-room-menu .menu-item').css('display', 'none');
+		chatRoom.css('display', 'block');
+		$('.chat-room-body').scrollTop($('.chat-room-body')[0].scrollHeight);
+		
+	}
+	
 	// 채팅방 메뉴바 클릭
-	$(document).on('click','.menu-btn',function(e){
-		
-		e.preventDefault();
-		
-		var menuItem = $('.menu-item')
-		
-		if(menuItem.css('display') == 'none'){
+	$('.menu-btn').click(function(e){
+		var menuItem = $(this).next('.menu-item');
+		if(menuItem.is(':hidden')){
 			menuItem.css('display', 'block')
-		}else if(menuItem.css('display') == 'block'){
+		}else{
 			menuItem.css('display', 'none')
 		}
-		
 	})
 	
 	// 채팅방 close 클릭
@@ -53,76 +60,34 @@ $(function(){
 		fn_chatRoomClose();
 	})
 	
-	// 새로운 대화방 생성시 참여자 라디오버튼 클릭, 네임태그 추가
-	$(document).on('click','input.chat-radio-btn',function(){
+	// 새로운 대화방 생성 Modal에서 참여자 버튼 클릭
+	$(document).on('click','input.chat-check-btn',function(){
 		
-		var index = $(this).parent().index('label')
-		var checkNum = $(this).siblings('input.radio-check-num')
 		var userInfo = $(this).closest('label')
-		
-		if(checkNum.val() == index) {
- 
-			checkNum.val('-1')
-			$(this).prop("checked", false);
-			fn_nameTag(userInfo, 'remove');
-			
+		var index = $('.chat-radio-user').index($(this).closest('.chat-radio-user'))
+		console.log(index)
+		if($(this).prop('checked')) { 
+			fn_nameTag(userInfo, index,'add');        	
          }else{
- 
-        	checkNum.val(''+index+'');
-        	fn_nameTag(userInfo, 'add');
-        	
+        	 fn_nameTag(userInfo, index,'remove');			
          }
 	})
 	
-	// 네임태그 클릭시 제거
-	$(document).on('click','.name-tag',function(){
-		
-		var tagCheckNum = $(this).find('.tag-check-num').val();
-		var radioBtn = $('.chat-radio-btn[name='+tagCheckNum+'')
-		var checkNum = radioBtn.siblings('input.radio-check-num')
-		
-		checkNum.val('-1')
-		radioBtn.prop("checked", false);
-		
-		$(this).remove();
-	})
-	
-	// 채팅방 상세보기
-	function fn_chatRoomView(e){
-		
-		var chatRoom = $('.chat-room');
-		chatRoom.find('.chat-room-title').text(e.title);
-		chatRoom.find('.chat-room-user').text(e.user);
-		chatRoom.css('display', 'block');
-		
-	}
-	
-	// 채팅방 close
-	function fn_chatRoomClose(){
-		
-		var chatRoom = $('.chat-room');
-		chatRoom.css('display', 'none')
-		
-	}
-	
-	// 채팅방 생성시 네임태그 추가,제거
-	function fn_nameTag(userInfo, action){
-		
-		
-		var nameTagClone = $('.name-tag-clone').clone();
-		var userName = userInfo.find('.chat-radio-user-name').text();
-		var userImage = userInfo.find('.chat-radio-user-image').attr('src');
-		var tagCheckName = userInfo.find('.chat-radio-btn').attr('name');
+	// fn_nameTag: 새로운 채팅방 유저 체크버튼 클릭시 네임태그 추가,제거
+	function fn_nameTag(userInfo, index, action){
 		
 		switch(action){
 		
 		case "add" :
 			
+			var nameTagClone = $('.name-tag-clone').clone();
+			var userName = userInfo.find('.chat-radio-user-name').text();
+			var userImage = userInfo.find('.chat-radio-user-image').attr('src');
+			
 			nameTagClone.find('.tag-user-image').attr('src',userImage);
 			nameTagClone.find('.tag-user-name').text(userName);
-			nameTagClone.find('.tag-check-num').val(tagCheckName)
+			nameTagClone.find('.tag-index').val(index);
 			
-			nameTagClone.addClass(tagCheckName);
 			nameTagClone.removeClass('name-tag-clone');
 			nameTagClone.css('display','inline-block');
 			
@@ -132,7 +97,8 @@ $(function(){
 			
 		case "remove" :
 			
-			$('.'+tagCheckName+'').remove();
+			var nameTag = $('.name-tag-area').find('.tag-index[value='+index+']')
+			nameTag.closest('.name-tag').remove();
 			
 			break;
 		
@@ -140,19 +106,83 @@ $(function(){
 		
 	}
 	
+	// 네임태그 클릭시 제거
+	$(document).on('click','.name-tag',function(){
+		
+		var index = $(this).find('.tag-index').val();
+		console.log(index)
+		$('.chat-radio-user').eq(index).find('.chat-check-btn').prop("checked", false);
+		
+		$(this).remove();
+	})
 	
 	
 	
 	
 	
 	
+	// 채팅방 close
+	function fn_chatRoomClose(){
+		
+		var chatRoom = $('.chat-room');
+		chatRoom.css('display', 'none')
+		
+	}
 	
 	
 	
-	
-	
-	
+	// 모달창 생성 클릭 이벤트
+	$('.modal-request').click(function(){
+		
+		if($(this).hasClass('chat-list-create-room')){
 			
+			fn_modalRequest('/createChatRoomModal', '#create-chat-room-modal');
 			
+		}else if($(this).hasClass('invite-user')){
 			
-});
+			fn_modalRequest('/inviteUserModal', '#invite-user-modal');
+			
+		}
+		
+	})
+	
+	// 모달 리퀘스트 함수
+	function fn_modalRequest(modalUrl, title, param){
+		
+		//param 변수가 undefined 라면 
+		if(param == undefined) param = {};
+		
+		var request = $.ajax({
+		  url: modalUrl,
+		  method: "GET",
+		  data: param,
+		  dataType: "html"
+		});
+		
+		request.done(function( data ) {
+			
+			$('body').append(data)
+			
+			//html 렌더링이 완료될때까지 대기
+			setTimeout(function(){
+				
+				$(title).on('hidden.bs.modal', function (e) {
+									
+					$('form').each(function() {
+					      this.reset();
+					});
+					$('.name-tag').remove();
+					
+				})
+				$(title).modal('show');
+				
+			}, 200);
+			
+		});
+		 
+		request.fail(function( jqXHR, textStatus ) {
+		  alert( "Request failed: " + textStatus );
+		}); 
+	
+	}
+	
